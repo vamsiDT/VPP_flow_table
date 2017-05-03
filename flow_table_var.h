fc0bfac60c7d297ab89fe942da01658ec325e60f
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-05-03 14:25:11 +0200

    new

---
+++ flow_table_var.h
@@ -0,0 +1,9 @@
+#include <vnet/ip/flow_table.h>
+
+flowcount_t *  nodet[TABLESIZE][4] ;
+flowcount_t *  head ;
+flowcount_t *  previousnode;
+flowcount_t *  tail;
+int numflows;
+int packets;
+int refresh = 15000000;
