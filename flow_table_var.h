fc781984e8a0bdc8d8b7e59af7578f0b0cbda8e7
Vamsi <f2013790@goa.bits-pilani.ac.in>
2017-05-29 15:03:01 +0200

    Update flow_table_var.h

--- flow_table_var.h
+++ flow_table_var.h
@@ -1,6 +1,7 @@
 #include <vnet/ip/flow_table.h>
 
 flowcount_t *  nodet[TABLESIZE] ;
+flowcount_t *  activeflows[VLIB_FRAME_SIZE];
 flowcount_t *  head ;
 flowcount_t *  previousnode;
 flowcount_t *  tail;
